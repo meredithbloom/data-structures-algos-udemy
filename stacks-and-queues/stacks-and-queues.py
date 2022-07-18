2675c2c4cec0332ff41db79564aa75db9247d3ac
Meredith Bloom <meredithbloom@Merediths-iMac.local>
2022-07-18 16:10:16 -0400

    added folder for stacks and queues, base notes file

---
+++ stacks-and-queues/stacks-and-queues.py
@@ -0,0 +1,24 @@
+#---- STACKS ----#
+# LIFO (last in, first out)
+
+# i.e. browser history
+# google
+# udemy
+# youtube
+
+# can build using either array OR linked list
+
+
+
+
+
+
+#---- QUEUES ----#
+# FIFO (first in, first out)
+
+# waitlist
+# matt -- joy -- samir -- pavel
+
+
+# avoid building with array at all costs - because arrays use indices to locate items, anytime you remove an item from the front of an array, the index numbers need to shift for every item
+# linked lists are much better to use when implementing a queue
