c82ff2b9b24eecfdd3d997ad698cabbe7d697cec
Meredith Bloom <meredithbloom@Merediths-iMac.local>
2022-07-25 21:52:33 -0400

    fibonacci

--- recursion/factorial.py
+++ recursion/factorial.py
@@ -2,7 +2,7 @@
 # write two functions that finds the factorial of any numbers. one should use recursion, the other should just use a for loop
 
 
-def factorial_recursive(number):
+def factorial_recursive(number): # O(n)
     if number > 1:
         return number*factorial_recursive(number-1)
     else:
@@ -10,7 +10,7 @@ def factorial_recursive(number):
         
 
 
-def factorial_iterative(number):
+def factorial_iterative(number): # O(n)
     total = 1
     for num in range(number, 0, -1):
         total*=num
