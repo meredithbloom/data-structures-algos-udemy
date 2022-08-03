223c79f42e92788143a71e9bcad3b6baef613fca
Meredith Bloom <31168207+meredithbloom@users.noreply.github.com>
2022-08-03 21:54:40 +0000

    added replit package files. bfs solution

--- searching/breadth-first-search.py
+++ searching/breadth-first-search.py
@@ -10,39 +10,66 @@
 '''
 #[9, 4, 20, 1, 6, 15, 170]
 
+print('please work')
+
 class Node:
-  def __init__(self,val):
-    self.val = val
-    self.left = None
-    self.right = None
+	def __init__(self, val):
+		self.val = val
+		self.left = None
+		self.right = None
 
 class BinarySearchTree:
 	def __init__(self):
-	  self.root = None
+		self.root = None
 
-  def insert(self, val):
-    new = Node(val)
-    if self.root == None:
-      self.root = new
-      return
-    temp = self.root
-    while True:
-      if new.val < temp.val:
-        if temp.left == None:
-          temp.left = new
-          break
-        else:
-          temp = temp.left
-      elif new.val > temp.val:
-        if temp.right == None:
-          temp.right = new
-          break
-        else:
-          temp = temp.right
-	
+	def insert(self, val):
+		new = Node(val)
+		if self.root == None:
+			self.root = new
+			return
+		temp = self.root
+		while True:
+			if new.val < temp.val:
+				if temp.left == None:
+					temp.left = new
+					break
+				else:
+					temp = temp.left
+			elif new.val > temp.val:
+				if temp.right == None:
+					temp.right = new
+					break
+				else:
+					temp = temp.right
+
+	def lookup(self, val):
+		temp = self.root
+		while True:
+			if temp.val == val:
+				return True
+			elif temp == None:
+				return False
+			elif val < temp.val:
+				temp = temp.left
+			elif val > temp.val:
+				temp = temp.right
+
 	def BFS(self):
-		return self.root
+		current = self.root
+		mylist = []
+		queue = []
+		queue.append(current)
 
+		while len(queue) > 0:
+			current = queue[0]
+			del queue[0]
+			mylist.append(current.val)
+			if current.left:
+				queue.append(current.left)
+			if current.right:
+				queue.append(current.right)
+
+		return mylist
 
 
 tree = BinarySearchTree()
@@ -53,8 +80,7 @@ tree.insert(20)
 tree.insert(170)
 tree.insert(15)
 tree.insert(1)
+
+x = tree.lookup(170)
+print(x)
 print(tree.BFS())
-
-
-
-	
